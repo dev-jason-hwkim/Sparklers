ba44144d378ce2c86a52e6e0274abdc4e4e4552c
HyunWoo <dev.hwkim@gmail.comgit>
2018-09-02 21:47:40 +0900

    모델 정리

---
+++ Sparclers/Sources/Models/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Sparclers
+//
+//  Created by HyunWoo-Kim on 2018. 9. 2..
+//  Copyright © 2018년 HyunWoo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Color {
+    var color = UIColor.white
+    var name = ""
+    var resource: UIImage?
+}
