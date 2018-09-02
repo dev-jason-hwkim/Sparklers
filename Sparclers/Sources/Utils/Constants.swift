fcaff1e678b32797125caf6acdf52f693bdb7312
HyunWoo <dev.hwkim@gmail.comgit>
2018-09-02 21:48:02 +0900

    Constatns추가

---
+++ Sparclers/Sources/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Sparclers
+//
+//  Created by HyunWoo-Kim on 2018. 9. 2..
+//  Copyright © 2018년 HyunWoo. All rights reserved.
+//
+
+import UIKit
+
+public let SCREEN_SIZE = CGSize(width: UIScreen.main.nativeBounds.size.width / UIScreen.main.nativeScale,
+                                height: UIScreen.main.nativeBounds.size.height / UIScreen.main.nativeScale)
+
+public let SCREEN_RATIO =  SCREEN_SIZE.width / 320.0
