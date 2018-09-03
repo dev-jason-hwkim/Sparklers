f9750cf638d0493b5281448927ec88904f8dd5e3
hwkim <hwkim@i-aurora.co.kr>
2018-09-03 18:38:02 +0900

    UICollectionView Rx Extension추가

---
+++ Sparclers/Sources/Rx/UICollectionView-Rx.swift
@@ -0,0 +1,20 @@
+//
+//  UICollectionView-Rx.swift
+//  Sparclers
+//
+//  Created by HyunWoo on 03/09/2018.
+//  Copyright © 2018 HyunWoo. All rights reserved.
+//
+
+import RxCocoa
+import RxDataSources
+import RxSwift
+
+extension Reactive where Base: UICollectionView {
+    func itemSelected<S>(dataSource: CollectionViewSectionedDataSource<S>) -> ControlEvent<S.Item> {
+        let source = self.itemSelected.map { indexPath in
+            dataSource[indexPath]
+        }
+        return ControlEvent(events: source)
+    }
+}
